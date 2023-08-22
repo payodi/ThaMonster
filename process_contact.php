0fcf838ff5806c7752d8cca6315855f2da4e816a
payodi <142627942+payodi@users.noreply.github.com>
2023-08-22 10:19:21 +0200

    none.

--- process_contact.php
+++ process_contact.php
@@ -1,19 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
 
-    // Envoi du message à l'adresse e-mail de destination (à personnaliser)
-    $to = "lolasubony@gmail.com";
-    $subject = "Nouveau message de contact de $name";
-    $headers = "From: $email";
-
-    // Envoi du message
-    mail($to, $subject, $message, $headers);
-
-    // Redirection vers la page de remerciement (à personnaliser)
-    header("Location: thankyou.html");
-    exit();
-}
-?>
